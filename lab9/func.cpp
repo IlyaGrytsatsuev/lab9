169e853093386c304dc012c11c0b8aff0c16cb99
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2021-05-15 18:17:27 +0300

    input in main

--- lab9/func.cpp
+++ lab9/func.cpp
@@ -3,144 +3,55 @@
 #include "func.hpp"
 #include <stdlib.h>
 
-void input(int *size, float** k, int* p)
-{
-    float koeff;
-    int num;
-    int i = 0;
-    int degree;
-    printf("enter the power:\n");
-    scanf("%d", &num);
-    if(num == 0){
-        *size = 1;
-    }
-    if(num > 0)
-    {
-        *size = num + 1 ;
-    }
-    if(num < 0)
-    {
-        *size = num*-1 + 1 ;
-    }
-    degree = num;
-    *p = degree;
-    float* kf = (float*)malloc(*size*sizeof(float));
-    if(kf == NULL)
-    {
-        printf("Error");
-        exit(-1);
-    }
-    *k = kf;
 
-   
-    if(degree == 0)
-    {
-        for(i = 0; i<*size; i++)
-        {
-            printf("enter the coefficient x^%d:\n", degree);
-            scanf("%f", &koeff);
-            kf[i] = koeff;
-        }
-    }
-
-    if(degree > 0)
-    {
-        for(i = 0; i<*size; i++)
-        {
-            printf("enter the coefficient x^%d:\n", degree);
-            scanf("%f", &koeff);
-            kf[i] = koeff;
-            degree -- ;
-        }
-        degree++;
-    }
-
-    if(degree < 0)
-    {
-        for(i = 0; i<*size; i++)
-        {
-            printf("enter the coefficient x^%d:\n", degree);
-            scanf("%f", &koeff);
-            kf[i] = koeff;
-            degree ++ ;
-        }
-    }
-}
-
-void derivative(int* size, float* k, int* p, float** d)
+void derivative(int size, float* k, int* p, float* d)
 {
     int i = 0;
-    float der;
-    float* der1 = (float*)malloc(*size*sizeof(float));
     int degree = *p;
-    
-    if(der1 == NULL)
-    {
-        printf("Error");
-        exit(-1);
-    }
-    *d = der1;
     if(*p == 0)
     {
-        for (i = 0; i < *size; i++)
+        for (i = 0; i < size; i++)
         {
-            der = k[i]*(float)degree;
-            der1[i] = der;
+            d[i] = k[i]*(float)degree;
         }
     }
+    
     if(*p > 0)
     {
         *p = degree - 1;
-        for (i = 0; i < *size; i++)
+        for (i = 0; i < size; i++)
         {
-            der = k[i]*(float)degree;
-            der1[i] = der;
+            d[i] = k[i]*(float)degree;
             degree --;
         }
     }
+    
     if( *p < 0)
     {
         *p = degree - 1;
-        for (i = 0; i < *size; i++)
+        for (i = 0; i < size; i++)
         {
-            der = k[i]*(float)degree;
-            der1[i] = der;
+            d[i] = k[i]*(float)degree;
             degree ++;
         }
     }
 }
-void multiply (int* p1,int* size1, float* d1, int* p2, int* size2, float* d2,float** r, int** po)
+
+void multiply (int p1, int size1, float* d1, int p2, int size2, float* d2,float* r, int* po)
 {
-    float num = 0;
     int count = 0;
-    int deg1 = *p1;
-    int deg2 = *p2;
+    int deg1 = p1;
+    int deg2 = p2;
     int i = 0;
     int j = 0;
-    float* rez = (float*)malloc(*size1**size2*sizeof(float));
     
-    if(rez == NULL)
-    {
-        printf("Error");
-        exit(-1);
-    }
-    *r = rez;
-    int* pow = (int*)malloc(*size1**size2*sizeof(int));
     
-    if(pow == NULL)
+    for ( i = 0; i < size1; i++ )
     {
-        printf("Error");
-        exit(-1);
-    }
-    *po = pow;
-    
-    for ( i = 0; i < *size1; i++ )
-    {
-        for (j=0;j < *size2; j++)
+        for (j=0;j < size2; j++)
         {
-            num = d1[i]*d2[j];
-            rez[count] = num;
-            pow[count] = deg1+deg2;
+            r[count] = d1[i]*d2[j];
+            po[count] = deg1+deg2;
             if (deg2>0){
                 deg2--;
             }
@@ -149,7 +60,7 @@ void multiply (int* p1,int* size1, float* d1, int* p2, int* size2, float* d2,flo
             }
             count++;
         }
-        if (deg1>0)
+        if (p1>0)
         {
             deg1--;
         }
@@ -157,16 +68,17 @@ void multiply (int* p1,int* size1, float* d1, int* p2, int* size2, float* d2,flo
         {
             deg1++;
         }
-        deg2 = *p2;
+        deg2 = p2;
         }
     }
 
 
-void output(float* r, int* po, int* size1, int* size2)
+void output(float* r, int* po, int size1, int size2)
 {
     printf("\n");
     printf("Rezult:\n");
-    for (int i = 0; i < *size1**size2; i++ )
+    
+    for (int i = 0; i < size1*size2; i++ )
     {
         if(i == 0)
         {
--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,36 +1,177 @@
-
 #include <stdio.h>
 #include "func.hpp"
 #include <stdlib.h>
+
 int main()
 {
-    float* k;
-    float* k2;
     int p1 = 0;
     int p2 = 0;
     int size1 = 0;
     int size2 = 0;
-    int* po;
-    float* r;
-    float* d1;
-    float* d2;
+    int num;
+    int i = 0;
+    int degree1 = 0;
+    int degree2 = 0;
     
-    input(&size1, &k, &p1);
-    input(&size2, &k2,  &p2);
-    derivative (&size1, k, &p1,&d1);
-    derivative (&size2, k2, &p2, &d2);
-    multiply (&p1, &size1, d1, &p2, &size2, d2, &r, &po);
-    output (r, po, &size1, &size2);
-   // for(int i = 0; i < size1*size2 ; i++){
-    //    printf("%f ", r[i]);
-   // }
-   // for(int i = 0; i < size2 ; i++){
-    //    printf("%f ", d2[i]);
-    //}
-   // printf("%d ",p1 );
-   // printf("\n");
-   // for(int i = 0; i < size1*size2 ; i++){
-    //    printf("%d ", po[i]);
-    //}
+    printf("enter the power:\n");
+    scanf("%d", &num);
+    
+    if(num == 0)
+    {
+        size1 = 1;
+    }
+    
+    if(num > 0)
+    {
+        size1 = num + 1 ;
+    }
+    
+    if(num < 0)
+    {
+        size1 = num*-1 + 1 ;
+    }
+    
+    degree1 = num;
+    p1 = degree1;
+    
+    float* k1 = (float*)malloc(size1*sizeof(float));
+    if(k1 == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+
+    if(p1 == 0)
+    {
+        for(i = 0; i<size1; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree1);
+            scanf("%f", &k1[i]);
+        }
+    }
+
+    if(p1 > 0)
+    {
+        for(i = 0; i<size1; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree1);
+            scanf("%f", &k1[i]);
+            degree1 -- ;
+        }
+    }
+
+    if(p1 < 0)
+    {
+        for(i = 0; i<size1; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree1);
+            scanf("%f", &k1[i]);
+            degree1 ++ ;
+        }
+    }
+
+
+
+    printf("enter the power:\n");
+    scanf("%d", &num);
+
+    if(num == 0)
+    {
+        size2 = 1;
+    }
+
+    if(num > 0)
+    {
+        size2 = num + 1 ;
+    }
+
+    if(num < 0)
+    {
+        size2 = num*-1 + 1 ;
+    }
+
+    degree2 = num;
+    p2 = degree2;
+    
+    float* k2 = (float*)malloc(size2*sizeof(float));
+    if(k2 == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+
+
+    if(p2 == 0)
+    {
+        for(i = 0; i<size2; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree2);
+            scanf("%f", &k2[i]);
+        }
+    }
+
+    if(p2 > 0)
+    {
+        for(i = 0; i<size2; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree2);
+            scanf("%f", &k2[i]);
+            degree2 -- ;
+        }
+        degree2++;
+    }
+
+    if(p2 < 0)
+    {
+        for(i = 0; i<size2; i++)
+        {
+            printf("enter the coefficient x^%d:\n", degree2);
+            scanf("%f", &k2[i]);
+            degree2 ++ ;
+        }
+    }
+
+    
+    float* d1 = (float*)malloc(size1*sizeof(float));
+    
+    if(d1 == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    
+    float* d2 = (float*)malloc(size2*sizeof(float));
+    
+    if(d2 == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    
+    float* r = (float*)malloc(size1*size2*sizeof(float));
+    if(r == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    
+    int* po = (int*)malloc(size1*size2*sizeof(int));
+    if(po == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    derivative (size1, k1, &p1, d1);
+    derivative (size2, k2, &p2, d2);
+    multiply (p1, size1, d1, p2, size2, d2, r, po);
+    output (r, po, size1, size2);
+   
+   
+    free(k1);
+    free(k2);
+    free(po);
+    free(r);
+    free(d1);
+    free(d2);
     return 0;
 }
