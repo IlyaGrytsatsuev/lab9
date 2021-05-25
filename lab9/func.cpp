f1234912a91d4e4fc2032e46828b414021966675
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2021-05-25 20:35:16 +0300

    ACCEPTED

--- lab9/func.cpp
+++ lab9/func.cpp
@@ -7,13 +7,14 @@ void derivative(float* k, int* p, float* d)
 {
     int i = 0;
     int size = *p+1;
-    int count = 1;
+    
     d[0] = k[1];
+    
     for (i = 2; i < size; i++)
     {
         d[i-1] = k[i]*(float)i;
-        count ++;
     }
+    
     *p-=1;
 }
 
@@ -22,9 +23,9 @@ void multiply(float* d1, int p1, float* d2, int p2, float* r)
     p1+=1;
     p2+=1;
 
-    for (int i=0; i<p1; i++)
+    for (int i=0; i < p1; i++)
     {
-        for (int j=0; j<p2; j++)
+        for (int j=0; j < p2; j++)
         {
             r[i+j] += d1[i]*d2[j];
         }
--- lab9/main.cpp
+++ lab9/main.cpp
@@ -59,18 +59,77 @@ int main()
         printf("Error");
         exit(-1);
     }
-
+    
+    float* mult = (float*)malloc(((p1+1)+(p2+1)-1)*sizeof(float));
+    if(mult == NULL)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    int po = (p1+1)+(p2+1)-1;
+    multiply(k1, p1, k2, p2, mult);
+    printf("Multiplication of source polynom:\n");
+    output(mult, po);
     derivative(k1, &p1, d1);
     derivative(k2, &p2, d2);
-    int po = (p1+1)+(p2+1)-1;
-    float* r = (float*)malloc(((p1+1)+(p2+1)-1)*sizeof(float));
+    size1 = p1+1;
+    size2 = p2+1;
     
+    printf("The first derivative is:\n");
+    for (int i = 0; i < size1; i++ )
+    {
+        if(i == 0)
+        {
+            printf("%f", d1[i]);
+        }
+        else
+        {
+            
+            if(d1[i] < 0)
+            {
+                printf("%fx^%d", d1[i], i);
+            }
+            if (d1[i] > 0)
+            {
+                printf(" + %fx^%d", d1[i], i);
+            }
+        }
+    }
+    printf("\n\n");
+    
+    printf("The second derivative is:\n");
+    for (int i = 0; i < size2; i++ )
+    {
+        if(i == 0)
+        {
+            printf("%f", d2[i]);
+        }
+        else
+        {
+            
+            if(d2[i] < 0)
+            {
+                printf("%fx^%d", d2[i], i);
+            }
+            if (d2[i] > 0)
+            {
+                printf(" + %fx^%d", d2[i], i);
+            }
+        }
+    }
+    printf("\n\n");
+    
+     po = (p1+1)+(p2+1)-1;
+    float* r = (float*)malloc(((p1+1)+(p2+1)-1)*sizeof(float));
     if(r == NULL)
     {
         printf("Error");
         exit(-1);
     }
+    
     multiply(d1, p1, d2, p2, r);
+    
+    
     output(r, po);
    
     return 0;
